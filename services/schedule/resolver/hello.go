a5ccc4c7792482605fd93c7abeda8d1f4ac7498c
Water.Li <lizongbin@hotmail.com>
2023-04-10 18:35:44 +0800

    backup

--- services/schedule/resolver/hello.go
+++ services/schedule/resolver/hello.go
@@ -3,3 +3,7 @@ package resolver
 func (*Resolver) Hello() string {
 	return "Hello query!"
 }
+
+func (*Resolver) Hello2() string {
+	return "Hello query 2!"
+}
--- services/schedule/schema/schema.go
+++ services/schedule/schema/schema.go
@@ -3,9 +3,14 @@ package schema
 var SDL = `
 	schema {
 		query: Query
+		mutation: Mutation
 	}
 
 	type Query {
 		hello: String!
+	}
+
+	type Mutation {
+		hello2: String!
 	}
 `
