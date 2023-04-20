9ad335a7f04cf5e66b6e0f58ad76aa46903f1c1d
Water.Li <lizongbin@hotmail.com>
2023-04-20 17:21:49 +0800

    Update observer.go

--- services/models/logs/observer.go
+++ services/models/logs/observer.go
@@ -3,7 +3,6 @@ package logs
 import (
 	"context"
 
-	"github.com/codebdy/entify/model/graph"
 	"github.com/codebdy/entify/model/observer"
 )
 
@@ -23,17 +22,17 @@ func (o *ModelObserver) Key() string {
 	return o.key
 }
 
-func (o *ModelObserver) ObjectPosted(object map[string]interface{}, entity *graph.Entity, ctx context.Context) {
-	WriteModelLog(&entity.Class, ctx, UPSERT, SUCCESS, ctx.Value("gql"))
+func (o *ModelObserver) ObjectPosted(object map[string]interface{}, entityName string, ctx context.Context) {
+	//WriteModelLog(&entity.Class, ctx, UPSERT, SUCCESS, ctx.Value("gql"))
 }
 
-func (o *ModelObserver) ObjectMultiPosted(objects []map[string]interface{}, entity *graph.Entity, ctx context.Context) {
-	WriteModelLog(&entity.Class, ctx, MULTI_UPSERT, SUCCESS, ctx.Value("gql"))
+func (o *ModelObserver) ObjectMultiPosted(objects []map[string]interface{}, entityName string, ctx context.Context) {
+	//WriteModelLog(&entity.Class, ctx, MULTI_UPSERT, SUCCESS, ctx.Value("gql"))
 }
-func (o *ModelObserver) ObjectDeleted(object map[string]interface{}, entity *graph.Entity, ctx context.Context) {
-	WriteModelLog(&entity.Class, ctx, DELETE, SUCCESS, ctx.Value("gql"))
+func (o *ModelObserver) ObjectDeleted(object map[string]interface{}, entityName string, ctx context.Context) {
+	//WriteModelLog(&entity.Class, ctx, DELETE, SUCCESS, ctx.Value("gql"))
 }
 
-func (o *ModelObserver) ObjectMultiDeleted(objects []map[string]interface{}, entity *graph.Entity, ctx context.Context) {
-	WriteModelLog(&entity.Class, ctx, MULTI_DELETE, SUCCESS, ctx.Value("gql"))
+func (o *ModelObserver) ObjectMultiDeleted(objects []map[string]interface{}, entityName string, ctx context.Context) {
+	//WriteModelLog(&entity.Class, ctx, MULTI_DELETE, SUCCESS, ctx.Value("gql"))
 }
