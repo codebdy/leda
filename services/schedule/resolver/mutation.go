c1d6d3529e4694f4bb0730bf0e1ca6b9532396d0
Water.Li <lizongbin@hotmail.com>
2023-04-28 11:55:43 +0800

    Update mutation.go

--- services/schedule/resolver/mutation.go
+++ services/schedule/resolver/mutation.go
@@ -1,26 +1,26 @@
 package resolver
 
 import (
+	"context"
 	"strconv"
 
 	"codebdy.com/leda/services/schedule/runner"
-	"github.com/graphql-go/graphql"
 )
 
-func (*Resolver) CreateTask(p graphql.ResolveParams) string {
+func (*Resolver) CreateTask(id string, ctx context.Context) string {
 	//runner.TaskRunner.StartTask()
 	return "CreateTask !"
 }
 
-func (*Resolver) StartTask(p graphql.ResolveParams) string {
+func (*Resolver) StartTask(id string, ctx context.Context) string {
 	//runner.TaskRunner.StartTask()
 	return "StartOneShotTask !"
 }
 
-func (*Resolver) StopTask(p graphql.ResolveParams) string {
-	id, err := strconv.ParseInt(p.Args["id"].(string), 10, 64)
+func (*Resolver) StopTask(id string, ctx context.Context) string {
+	numId, err := strconv.ParseInt(id, 10, 64)
 	if err == nil {
-		runner.TaskRunner.StopTask(id)
+		runner.TaskRunner.StopTask(numId)
 	}
 
 	return "cancelOneShotTask !"
